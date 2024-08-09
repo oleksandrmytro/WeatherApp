1815078f536e2f0caa1449bf2593c1e0e220af76
Волерій Ольбертович < @DESKTOP-0P72KBD>
2024-08-09 17:10:53 +0200

    Add project files.

---
+++ WeatherApp/ViewModel/Helpers/AccuWheatherHelper.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel.Helpers
+{
+    public class AccuWheatherHelper
+    {
+        public const string BASE_URL = "http://dataservice.accuweather.com/";
+        public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+        public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
+        public const string API_KEY = "Oy9YcFrLWZAqzMttp8AiPzhZr7jyuAxH";
+
+        public static async Task<List<City>> GetCities(string query)
+        {
+            List<City> cities = new List<City>();
+
+            string url = BASE_URL + string.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query);
+
+            using(HttpClient client = new HttpClient())
+            {
+                var respons = await client.GetAsync(url);
+                string json = await respons.Content.ReadAsStringAsync();
+
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+
+            return cities;
+        }
+
+        public static async Task<CurrentCoditions> GetCurrentCoditions(string cityKey)
+        {
+            CurrentCoditions currentCoditions = new CurrentCoditions();
+
+            string url = BASE_URL + string.Format(CURRENT_CONDITIONS_ENDPOINT, cityKey, API_KEY);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var respons = await client.GetAsync(url);
+                string json = await respons.Content.ReadAsStringAsync();
+
+                currentCoditions = (JsonConvert.DeserializeObject<List<CurrentCoditions>>(json)).FirstOrDefault();
+            }
+
+            return currentCoditions;
+        }
+    }
+}
---
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set 
+            { 
+                query = value; 
+                OnPropertyChanged("Query");
+            }
+        }
+
+        private CurrentCoditions currentCoditions;
+
+        public CurrentCoditions CurrentCoditions
+        {
+            get { return currentCoditions; }
+            set 
+            { 
+                currentCoditions = value; 
+                OnPropertyChanged("CurrentCoditions"); ;
+            }
+
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set 
+            { 
+                selectedCity = value; 
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+        public WeatherVM()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject())) {
+                SelectedCity = new City
+                {
+                    LocalizedName = "New York"
+                };
+                CurrentCoditions = new CurrentCoditions
+                {
+                    WeatherText = "Partly Cloudy",
+                    Temperature = new Temperature
+                    {
+                        Metric = new Units
+                        {
+                            Value = 22
+                        }
+                    }
+                };
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
